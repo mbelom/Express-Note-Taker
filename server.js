4b4a823afe2e12247da54661d93a1919a104cc23
Marissa Belombre <mbelo@MJBGALAXY>
2022-09-04 19:38:04 -0700

    edits in server.js

--- server.js
+++ server.js
@@ -9,32 +9,32 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 
 //serve images, CSS files, and JavaScript files in a directory named public
-app.use(express.static('public'));
+app.use(express.static('./Develop/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
 //route to notes.html
 app.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "/public/notes.html"));
+    res.sendFile(path.join(__dirname, "./Develop/public/notes.html"));
 });
 
 //route to read the `db.json` file and return all saved notes as JSON.
 app.get("/api/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "/db/db.json"));
+    res.sendFile(path.join(__dirname, "./Develop/db/db.json"));
 });
 
 //route to index.html aka main page
 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "/public/index.html"));
+    res.sendFile(path.join(__dirname, "./Develop/public/index.html"));
 });
 
 //receive a new note to save on the request body, add it to the `db.json` file, 
 //and then return the new note to the client.
 app.post("/api/notes", (req, res) => {
     let newNote = req.body;
-    let noteList = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    let noteList = JSON.parse(fs.readFileSync("./Develop/db/db.json", "utf-8"));
     let notelength = (noteList.length).toString();
 
     //create new property called id based on length and assign it to each json object
@@ -43,13 +43,13 @@ app.post("/api/notes", (req, res) => {
     noteList.push(newNote);
 
     //write the updated data to db.json
-    fs.writeFileSync("./db/db.json", JSON.stringify(noteList));
+    fs.writeFileSync("./Develop/db/db.json", JSON.stringify(noteList));
     res.json(noteList);
 })
 
 //delete note according to their tagged id.
 app.delete("/api/notes/:id", (req, res) => {
-    let noteList = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    let noteList = JSON.parse(fs.readFileSync("./Develop/db/db.json", "utf8"));
     let noteId = (req.params.id).toString();
 
     //filter all notes that does not have matching id and saved them as a new array
@@ -59,7 +59,7 @@ app.delete("/api/notes/:id", (req, res) => {
     })
 
     //write the updated data to db.json and display the updated note
-    fs.writeFileSync("./db/db.json", JSON.stringify(noteList));
+    fs.writeFileSync("./Develop/db/db.json", JSON.stringify(noteList));
     res.json(noteList);
 });
 
